4746bd014f489047dede387705218aee34fe2df5
jackparzick <jackparzick@gmail.com>
2025-12-29 15:47:33 -0700

    Parzick identity: wrapper header + library.properties + example includes

---
+++ src/PortentaUWBShieldParzick.h
@@ -0,0 +1,9 @@
+#ifndef PORTENTA_UWB_SHIELD_PARZICK_H
+#define PORTENTA_UWB_SHIELD_PARZICK_H
+#warning "Compiling PortentaUWBShieldParzick fork"
+
+// Public include header for the Parzick fork.
+// This includes the fork's own PortentaUWBShield.h (in this same library folder).
+#include "PortentaUWBShield.h"
+
+#endif // PORTENTA_UWB_SHIELD_PARZICK_H
